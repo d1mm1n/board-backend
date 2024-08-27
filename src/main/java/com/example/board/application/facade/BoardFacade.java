c4d09e21da9859b26102b4194942bd4b90e7015b
홍지민 <dimmining@naver.com>
2024-08-27 23:09:25 +0900

    Feat/#11: 게시물 조회 구현

--- src/main/java/com/example/board/application/facade/BoardFacade.java
+++ src/main/java/com/example/board/application/facade/BoardFacade.java
@@ -1,6 +1,7 @@
 package com.example.board.application.facade;
 
 
+import com.example.board.application.dto.response.BoardDetailResponse;
 import com.example.board.domain.board.entity.Board;
 import com.example.board.domain.board.service.BoardService;
 import com.example.board.domain.member.entity.Member;
@@ -8,6 +9,8 @@ import com.example.board.domain.member.service.MemberService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,6 +25,19 @@ public class BoardFacade {
         Member member = memberService.findById(1L); //첫번째 회원가입한 회원만 글을 작성할 수 있음.
         Board board = new Board(member, title, content);
         boardService.save(board);
+    }
+    public BoardDetailResponse getBoard(long id) {
+        Board board = boardService.findById(id);
+
+        String title = board.getTitle(); //제목 가져오기
+        String content = board.getContent(); // 작성 내용 가져오기
+        LocalDateTime createdAt = board.getCreatedAt(); //작성 일자 가져오기
+        int view=board.getView()+1; //조회수 늘려주기
+        Member member = board.getMember(); //작성자 정보 가져오기
+
+        return new BoardDetailResponse(
+                title,content,view,createdAt
+        );
     }
 
 }
