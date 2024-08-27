b760508b5841da42db6e9180a26915b71d360e83
홍지민 <dimmining@naver.com>
2024-08-27 22:39:59 +0900

    Feat/#9: 게시물 등록 구현

---
+++ src/main/java/com/example/board/application/dto/request/BoardWriteRequest.java
@@ -0,0 +1,13 @@
+package com.example.board.application.dto.request;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardWriteRequest {
+    private String title;
+    private String content;
+}
